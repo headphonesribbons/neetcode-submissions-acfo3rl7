9effe9d943449441c15cd23b4ae8f3f43af73182
xinlin <megantxl1@gmail.com>
2026-03-30 21:56:49 +0800

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int len = strs.length;
+        List[] keys = new List[len]; // keys for later
+        HashMap<List<String>, List<String>> anagrams = new HashMap<>(); // for checking anagrams
+        for (int i = 0; i < len; i++) { // traverse array
+            String word = strs[i];
+            int[] letters = new int[26]; // alphabet array
+            for (int j = 0; j < word.length(); j++) { // to make freq array for string
+                int letter = word.charAt(j) - 'a';
+                letters[letter]++;
+            }
+            List<String> key = new ArrayList<>(); // make the key
+            for (int k = 0; k < 26; k++) { // to make key, we create the sorted letters
+                if (letters[k] != 0) { // if this letter exists,
+                    while (letters[k] > 0) { // add it all.
+                        key.add(String.valueOf(k));
+                        letters[k]--;
+                    }
+                }
+            }
+            if (anagrams.containsKey(key)) { // if this is an anagram of something we've seen,
+                List<String> list = anagrams.get(key);
+                list.add(word); // append the curr string to our list
+                anagrams.replace(key, list);
+            } else { // this is a new thing!
+                List<String> list = new ArrayList<>(); // create a new list
+                list.add(word); // append the curr string to that list
+                anagrams.put(key, list); // put it into the map
+                keys[i] = key;
+            }
+        }
+        List<List<String>> answer = new ArrayList<>();
+        for (int i = 0; i < len; i++) { // traverse thru keys
+            if (keys[i] != null) { // if key exists,
+                List<String> key = keys[i]; // get the key.
+                List<String> words = anagrams.get(key); // get the anagram list that corresponds,
+                answer.add(words); // and add it to the answer!
+            }
+        }
+        return answer;
+    }
+}
